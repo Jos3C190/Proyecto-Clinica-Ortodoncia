e5f841138739e0825c8b09ac0eb8456ba3931bd8
Jos3C190 <josecarloslopezm17@gmail.com>
2025-06-08 15:47:09 -0600

    Update

--- routes/pagoRoutes.js
+++ routes/pagoRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pagoController = require('../controllers/pagoController');
-const verifyToken = require('../middleware/auth');
+const verifyToken = require('../middleware/auth')();
 
 router.get('/', verifyToken, pagoController.getAllPagos);
 router.post('/', verifyToken, pagoController.createPago);
