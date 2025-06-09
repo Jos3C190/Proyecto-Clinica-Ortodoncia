120f6c45f36ca7815888c3dd8d7a278a3b018d4d
Jos3C190 <josecarloslopezm17@gmail.com>
2025-06-09 11:41:39 -0600

    Agregar validaciones opcionales para la actualización de odontólogos, incluyendo campos como nombre, apellido, correo, teléfono, especialidad, fecha de nacimiento y contraseña. Esto mejora la flexibilidad en la actualización de datos de odontólogos.

--- routes/odontologoRoutes.js
+++ routes/odontologoRoutes.js
@@ -21,6 +21,16 @@ const validacionesOdontologo = [
     body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'), // Añadimos validación de password
 ];
 
+const validacionesOdontologoUpdate = [
+    body('nombre').optional().not().isEmpty().withMessage('El nombre es obligatorio'),
+    body('apellido').optional().not().isEmpty().withMessage('El apellido es obligatorio'),
+    body('correo').optional().isEmail().withMessage('El correo debe ser válido'),
+    body('telefono').optional().isMobilePhone().withMessage('El teléfono debe ser válido'),
+    body('especialidad').optional().not().isEmpty().withMessage('La especialidad es obligatoria'),
+    body('fecha_nacimiento').optional().isDate().withMessage('La fecha de nacimiento debe ser una fecha válida'),
+    body('password').optional().isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+];
+
 const validacionId = [
     param('id').isMongoId().withMessage('El ID debe ser un MongoID válido')
 ];
@@ -29,7 +39,7 @@ const validacionId = [
 router.post('/', auth(['admin']), validacionesOdontologo, crearOdontologo); // Solo admin crea odontólogos/administradores
 router.get('/', auth(['odontologo', 'admin']), obtenerOdontologos); // Odontólogos y admins ven la lista
 router.get('/:id', auth(['odontologo', 'admin']), validacionId, obtenerOdontologoPorId); // Odontólogos y admins ven detalles
-router.put('/:id', auth(['admin']), validacionId.concat(validacionesOdontologo), actualizarOdontologo); // Solo admin actualiza
+router.put('/:id', auth(['admin']), validacionId.concat(validacionesOdontologoUpdate), actualizarOdontologo); // Solo admin actualiza
 router.delete('/:id', auth(['admin']), validacionId, eliminarOdontologo); // Solo admin elimina
 
 module.exports = router;
